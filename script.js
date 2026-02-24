712031b6bfb780e6c84db0eba5bce7b14016555b
SazidHasan2003 <sarkersazid123@gmail.com>
2026-02-24 15:35:13 +0600

    selecting all element from html by js dom

---
+++ script.js
@@ -0,0 +1,10 @@
+const jobCards = document.getElementsByClassName("job-card");
+
+const interviewTab = document.getElementById("interviewTab");
+const rejectedTab = document.getElementById("rejectedTab");
+const allTab = document.getElementById("allTab");
+
+const interviewCount = document.getElementById("interviewCount");
+const rejectedCount = document.getElementById("rejectedCount");
+const totalCount = document.getElementById("totalCount");
+const availableCount = document.getElementById("availableCount");
