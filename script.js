277ad8a8cb28d2c2c8af95ab5ceb5dd2a003b902
SazidHasan2003 <sarkersazid123@gmail.com>
2026-02-24 15:40:36 +0600

    implimenting the job status counting logic

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ const interviewCount = document.getElementById("interviewCount");
 const rejectedCount = document.getElementById("rejectedCount");
 const totalCount = document.getElementById("totalCount");
 const availableCount = document.getElementById("availableCount");
+
+// ================= UPDATE COUNTS =================
+function updateCounts() {
+  let total = 0;
+  let interview = 0;
+  let rejected = 0;
+
+  for (let i = 0; i < jobCards.length; i++) {
+    const status = jobCards[i].getAttribute("data-status");
+
+    if (status !== "deleted") {
+      total++;
+    }
+
+    if (status === "interview") {
+      interview++;
+    }
+
+    if (status === "rejected") {
+      rejected++;
+    }
+  }
+
+  totalCount.innerText = total;
+  availableCount.innerText = total + " jobs";
+  interviewCount.innerText = interview;
+  rejectedCount.innerText = rejected;
+}
